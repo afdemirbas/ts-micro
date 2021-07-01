eedad839a59dd343332b1b37188610b6c4690876
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2021-07-01 20:27:11 +0300

    config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,25 @@
-export default {};
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const {
+  NODE_ENV = 'development',
+  PORT = 5000,
+  HOST = '0.0.0.0',
+  MONGODB_URL = 'mongodb://127.0.0.1:27017/mydb'
+} = process.env;
+
+export default {
+  env: NODE_ENV,
+  port: PORT,
+  host: HOST,
+  mongoose: {
+    url: MONGODB_URL,
+    options: {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  }
+};
