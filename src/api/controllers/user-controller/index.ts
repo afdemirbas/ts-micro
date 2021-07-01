012150704fb9bb150f612677510493021d7ec347
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2021-07-01 20:43:29 +0300

    controllers

--- src/api/controllers/user-controller/index.ts
+++ src/api/controllers/user-controller/index.ts
@@ -1,1 +1,9 @@
-export class UserController {}
+import express from "express"
+import { UserService } from '../../services/user-service';
+
+interface UserControllerInterface {
+  create: (req: express.Request, res: express.Response) => <Promise>
+}
+export class UserController implements UserControllerInterface {
+  create = async (req: express.Request, res: express.Response) => {};
+}
--- src/api/routes/v1/users.ts
+++ src/api/routes/v1/users.ts
@@ -1,5 +1,9 @@
 import express from 'express';
-
+import controllers from '../../controllers';
 const router = express.Router();
+
+router.get('/', controllers.UserController);
+router.post('/');
+router.put('/:id');
 
 export default router;
