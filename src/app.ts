364e450536f6364cfb9a8085f9a1e18d04505a3e
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2021-07-01 20:17:44 +0300

    routes v1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import users from './api/routes/v1/users';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/users', users);
+
+app.listen('');
