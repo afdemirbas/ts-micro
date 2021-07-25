62d04ea8ebd6db1fcdf11b3c78bbd63cf89e4a35
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2021-07-25 15:54:22 +0300

    Update index.ts

--- src/api/controllers/user-controller/index.ts
+++ src/api/controllers/user-controller/index.ts
@@ -1,9 +1,4 @@
-import express from "express"
+import express from 'express';
 import { UserService } from '../../services/user-service';
 
-interface UserControllerInterface {
-  create: (req: express.Request, res: express.Response) => <Promise>
-}
-export class UserController implements UserControllerInterface {
-  create = async (req: express.Request, res: express.Response) => {};
-}
+
